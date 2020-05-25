30f1fc0ad1df7825ccfbff8959fdf9e42a3d2a5c
ReniaJM <prana861@gmail.com>
2020-05-25 21:43:32 +0200

    add rwd

--- app/js/script.js
+++ app/js/script.js
@@ -4,21 +4,14 @@ const selectElement =(element)=>{
   return document.querySelector(element)
 };
 
-let menuToggle = selectElement('.menu-toggle');
 let body= selectElement('body');
 let headerTag = selectElement('header');
 let listTag = document.getElementById('list');
-let linkTag = listTag.getElementsByClassName('nav-link');
 let logo = document.getElementById('logo');
 let mainNavLinks = document.querySelectorAll("nav ul li a");
 
 
-
-
 // change scroll
-menuToggle.addEventListener('click', ()=>{
-  body.classList.toggle('open');
-});
 
 let scrolled=()=>{
   let dec = scrollY /(body.scrollHeight - innerHeight);
@@ -29,21 +22,20 @@ addEventListener('scroll',()=>{
   headerTag.style.setProperty('background', scrolled() > 2 ? "#252525" : "linear-gradient(to bottom, rgba(0,0,0,1), transparent)")
 });
 addEventListener('scroll',()=>{
-  logo.style.setProperty('font-size', scrolled() > 2 ? "4rem" : "3rem");
+  logo.style.setProperty('font-size', scrolled() > 2 ? "3rem" : "2.5rem");
   logo.style.setProperty('transition', scrolled() > 2 ? "font-size .5s" : "font-size .5s");
   mainNavLinks.forEach(link => {
-  link.style.setProperty('font-size', scrolled() > 2 ? "2rem" : "1.5rem");
+  link.style.setProperty('font-size', scrolled() > 2 ? "1.8rem" : "1.5rem");
   link.style.setProperty('transition', scrolled() > 2 ? "font-size .5s" : "font-size .5s");
   });
 });
 
 
 // pressed navigation
-window.addEventListener("scroll", event => {
+window.addEventListener("scroll", ()=> {
   let fromTop = window.scrollY;
   mainNavLinks.forEach(link => {
     let section = document.querySelector(link.hash);
-    console.log(section)
     if (
       section.offsetTop <= fromTop &&
       section.offsetTop + section.offsetHeight > fromTop
@@ -54,16 +46,6 @@ window.addEventListener("scroll", event => {
     }
   });
 });
-
-// pressed navigation
-
-for (var i = 0; i < linkTag.length; i++) {
-  linkTag[i].addEventListener("click", function () {
-    let current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace("active", "");
-    this.className += " active";
-  });
-}
 
 
 // scroll reveal
@@ -97,9 +79,29 @@ sr.reveal('.animate-bottom',{
   delay:600
 });
 
-// change color nav
 
 
+$(document).ready(function () {
+  $('.gallery li:lt(3)').show();
+  $('.less').hide();
+  var items =  9;
+  var shown =  3;
+  $('.more').click(function () {
+    $('.less').show();
+    shown = $('.gallery li:visible').length+3;
+    if(shown< items) {
+      $('.gallery li:lt('+shown+')').show(300);
+    } else {
+      $('.gallery li:lt('+items+')').show(300);
+      $('.more').hide();
+    }
+  });
+  $('.less').click(function () {
+    $('.gallery li').not(':lt(3)').hide(300);
+    $('.more').show();
+    $('.less').hide();
+  });
+});
 
 
 
